f26b1752818cf34b8e7afab0a309fb4d60de2d1f
Luke Mason <lukjmason@gmail.com>
2021-03-03 20:19:23 -0700

    Put in image for landing page

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -1,10 +1,14 @@
 import React, {useState} from 'react';
 import {Link} from 'react-router-dom';
+import './LandingPage.css';
 
 const LandingPage = () => {
   return (
     <section className='landing-page'>
-      <h1 className='site-title'>Charity Local</h1>
+      <div className='landing-page-header'>
+        <h1 className='site-title'>Charity Local</h1>
+        <h2 className='site-slogan'>Give back to your community. Donate locally.</h2>
+      </div>
     </section>
   )
 }
