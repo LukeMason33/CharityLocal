0fadeddb4437ab11af8194f2418cdafcc408724e
Luke Mason <lukjmason@gmail.com>
2021-03-04 21:06:04 -0700

    Create hooks for landing page form

--- src/components/LandingPageForm/LandingPageForm.js
+++ src/components/LandingPageForm/LandingPageForm.js
@@ -1,2 +1,12 @@
 import React, {useState} from 'react';
 import {Link} from 'react-router-dom';
+
+const LandingPageForm = () => {
+  const [category, setCategory] = useState('');
+  const [stateInput1, setStateInput1] = useState('');
+  const [cityInput1, setCityInput1] = useState('');
+  const [stateInput2, setStateInput2] = useState('');
+  const [cityInput2, setCityInput2] = useState('');
+}
+
+export default LandingPageForm;
