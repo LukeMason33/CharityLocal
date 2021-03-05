35e5d0dfbf16c79f7a18516c78abeaaabd2ce351
Luke Mason <lukjmason@gmail.com>
2021-03-04 23:30:44 -0700

    Do initial setup for custom charity searc section

--- src/components/LandingPageForm/LandingPageForm.js
+++ src/components/LandingPageForm/LandingPageForm.js
@@ -7,6 +7,30 @@ const LandingPageForm = () => {
   const [cityInput1, setCityInput1] = useState('');
   const [stateInput2, setStateInput2] = useState('');
   const [cityInput2, setCityInput2] = useState('');
+
+  return (
+    <form className='landing-page-form'>
+      <section className='select-category-section'>
+        <div className='select-category-input-container'>
+          <label for='select-category'>Choose a Category of Charity</label>
+          <select name='select-category' className='select-category'>
+          </seclect>
+        </div>
+        <div className='select-location-container'>
+          <div className='state-container'>
+            <label for='select-state'>State</label>
+            <select name='select-state' className='select-state'>
+            </select>
+          </div>
+          <div className='city-container'>
+            <label for='select-city'>City</label>
+            <select name='select-city' className='select-city'>
+            </select>
+          </div>
+        </div>
+      </section>
+    </form>
+  )
 }
 
 export default LandingPageForm;
