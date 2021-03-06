0d6344b9df1f11b8a8e373e157959bc3005c055a
Luke Mason <lukjmason@gmail.com>
2021-03-06 11:19:43 -0700

    Import fetch in App.js, call fetch in method fetchCharitiesByCategory

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,19 @@
 import React, {useState} from 'react';
 import {Route, Switch} from 'react-router-dom';
 import LandingPage from '../LandingPage/LandingPage.js';
+import {fetchLocalCharities} from '../../utilities.js';
 import './App.css';
 
 
 const App = () => {
+
+  const fetchCharitiesByCategory = (state, city, category) => {
+    fetchLocalCharities(state, city, category)
+      .then(response => {
+        console.log(response)
+      })
+  }
+
   return (
     <Switch>
       <Route
