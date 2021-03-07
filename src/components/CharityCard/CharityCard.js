940ae270274a6ab04382fa3d2f0d0258b300d61a
Luke Mason <lukjmason@gmail.com>
2021-03-07 15:36:04 -0700

    Get initial styling for charity cards

--- src/components/CharityCard/CharityCard.js
+++ src/components/CharityCard/CharityCard.js
@@ -7,18 +7,18 @@ const CharityCard = ({charity}) => {
   return (
     <article className='charity-card'>
       <div className='name-and-address-container'>
-        <h1 className='charity-name'><a href={charity.website}>{charity.name}</a></h1>
+        <h1><a className='charity-name' href={charity.website}>{charity.name}</a></h1>
         <div className='address-container'>
           <p className='state-and-city'>{charity.address.city}, {charity.address.stateOrProvince}</p>
           <p className='address'>{charity.address.streetAddress1} {charity.address.streetAddress2}, {charity.address.postalCode}</p>
         </div>
       </div>
       <div className='cause-container'>
-        <h2>Cause</h2>
+        <h2>Cause:</h2>
         <p className='charity-cause'>{charity.cause}</p>
       </div>
       <div className='mission-container'>
-        <h2>Mission</h2>
+        <h2>Mission:</h2>
         <p className='charity-mission'>{charity.mission}</p>
       </div>
     </article>
