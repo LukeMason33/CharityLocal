c727a9b090ea63668be3c12a10fa9e8ae5155127
Luke Mason <lukjmason@gmail.com>
2021-03-07 13:29:41 -0700

    Initial charityContainer setup

--- src/components/CharityContainer/CharityContainer.js
+++ src/components/CharityContainer/CharityContainer.js
@@ -1,1 +1,14 @@
 import React, {useState} from 'react';
+
+const CharityContainer = ({charities}) => {
+
+  const fillCharityCards () => {
+    charities.map(charity => < CharityCard charity={charity} />)
+  }
+
+  return (
+    <section className='charity-cards-section'>
+      {fillCharityCards()}
+    </section>
+  )
+}
