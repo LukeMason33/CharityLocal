03079ac4f54131fca541a4ad2cbfad6c428d6c5f
Luke Mason <lukjmason@gmail.com>
2021-03-07 15:09:38 -0700

    Style buttons on site

--- src/components/MainDashboard/MainDashboard.js
+++ src/components/MainDashboard/MainDashboard.js
@@ -8,15 +8,19 @@ const MainDashboard = ({charities}) => {
   return (
     <>
       <header className='dashboard-header'>
-        <Link to='/'>
-          <button className='to-landing-page-btn'>Back to Charity Search</button>
-        </Link>
-        <Link to='/about'>
-          <button className='to-about-btn'>About</button>
-        </Link>
-        <Link to='/mission'>
-          <button className='to-mission-btn'>Mission</button>
-        </Link>
+        <div className='back-btn-container'>
+          <Link to='/'>
+            <button className='to-landing-page-btn'>Back to Charity Search</button>
+          </Link>
+        </div>
+        <div className='page-btns-container'>
+          <Link to='/about'>
+            <button className='to-about-btn'>About</button>
+          </Link>
+          <Link to='/mission'>
+            <button className='to-mission-btn'>Mission</button>
+          </Link>
+        </div>
       </header>
       <section className='main-dashboard'>
         < CharityContainer charities={charities} />
