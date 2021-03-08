e2d83a616e2917247bfae832225296ba7f3b78a7
Luke Mason <lukjmason@gmail.com>
2021-03-07 18:33:41 -0700

    Initial setup for NotFoundPage component

---
+++ src/components/NotFoundPage/NotFoundPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+const NotFoundPage = () => {
+  return (
+    <>
+      <h1 className='header'>Sorry! This page does not exist!</h1>
+      <p className='description'>The page that you are trying to visit does not exist for this site! Click the button below to go to the landing page.</p>
+      <p className='description'>DONATE LOCALLY!</p>
+      <Link to='/'>
+        <button className='landing-page-btn'>Go To Landing Page</button>
+      </Link>
+    </>
+  )
+}
