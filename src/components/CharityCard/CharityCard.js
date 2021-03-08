33e04526af41434641bec3a68707dfbe3e6d6cf9
Luke Mason <lukjmason@gmail.com>
2021-03-08 15:46:36 -0700

    Fix loading page issue from about page

--- src/components/CharityCard/CharityCard.js
+++ src/components/CharityCard/CharityCard.js
@@ -1,5 +1,4 @@
-import React, {useState} from 'react';
-import {Link} from 'react-router-dom';
+import React from 'react';
 import './CharityCard.css';
 
 const CharityCard = ({charity}) => {
--- src/components/CharityContainer/CharityContainer.js
+++ src/components/CharityContainer/CharityContainer.js
@@ -1,11 +1,11 @@
-import React, {useState} from 'react';
+import React from 'react';
 import CharityCard from '../CharityCard/CharityCard.js';
 import './CharityContainer.css';
 
 const CharityContainer = ({charities}) => {
 
   const fillCharityCards = () => {
-    return charities.map(charity => < CharityCard charity={charity} />)
+    return charities.map((charity, index) => < CharityCard key={index} charity={charity} />)
   }
 
   return (
