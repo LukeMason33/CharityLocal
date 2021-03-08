58ade5197b00209bd2978b5ae770d54ce195608f
Luke Mason <lukjmason@gmail.com>
2021-03-08 16:15:11 -0700

    Align text in NotFoundPage component

--- src/components/NotFoundPage/NotFoundPage.js
+++ src/components/NotFoundPage/NotFoundPage.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import './NotFoundPage.css';
 
 const NotFoundPage = () => {
   return (
-    <>
+    <div className='not-found-container'>
       <h1 className='header'>Sorry! This page does not exist!</h1>
       <p className='description'>The page that you are trying to visit does not exist for this site! Click the button below to go to the landing page.</p>
       <p className='description'>DONATE LOCALLY!</p>
       <Link to='/'>
         <button className='landing-page-btn'>Go To Landing Page</button>
       </Link>
-    </>
+    </div>
   )
 }
 
