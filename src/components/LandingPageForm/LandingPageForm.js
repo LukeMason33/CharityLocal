7408cad3bab542549ec0643b8bcd337f6b643b46
Luke Mason <lukjmason@gmail.com>
2021-03-08 17:49:37 -0700

    Wrap select/inputs in labels

--- src/components/LandingPageForm/LandingPageForm.js
+++ src/components/LandingPageForm/LandingPageForm.js
@@ -36,36 +36,39 @@ const LandingPageForm = ({fetchCharitiesByCategory, error, clearError}) => {
           {errorMessage && <p className='error'>{errorMessage}</p>}
         </div>
         <div className='select-category-input-container'>
-          <label htmlFor='select-category'>Choose a Category of Charity</label>
-          <select
-            name='select-category'
-            className='select-category'
-            onChange={event => setCategory(event.target.value)}
-            onClick={clearErrorMessageAfterClick}>
-            <option value=''>--Select a Category--</option>
-            {fillOptionsForInputs(categoryData)}
-          </select>
+          <label>Choose a Category of Charity
+            <select
+              name='select-category'
+              className='select-category'
+              onChange={event => setCategory(event.target.value)}
+              onClick={clearErrorMessageAfterClick}>
+              <option value=''>--Select a Category--</option>
+              {fillOptionsForInputs(categoryData)}
+            </select>
+          </label>
         </div>
         <div className='select-location-container'>
           <div className='state-container'>
-            <label htmlFor='select-state'>State</label>
-            <select
-              name='select-state'
-              className='select-state'
-              onChange={event => setStateInput1(event.target.value)}>
-                <option value=''>--Select a State--</option>
-                {fillOptionsForInputs(stateData)}
-            </select>
+            <label htmlFor='select-state'>State
+              <select
+                name='select-state'
+                className='select-state'
+                onChange={event => setStateInput1(event.target.value)}>
+                  <option value=''>--Select a State--</option>
+                  {fillOptionsForInputs(stateData)}
+              </select>
+            </label>
           </div>
           <div className='city-container'>
-            <label htmlFor='select-city'>City</label>
-            <input
-              name='select-city'
-              className='select-city'
-              type='text'
-              placeholder='City Name'
-              onChange={event => setCityInput1(event.target.value)}>
-            </input>
+            <label htmlFor='select-city'>City
+              <input
+                name='select-city'
+                className='select-city'
+                type='text'
+                placeholder='City Name'
+                onChange={event => setCityInput1(event.target.value)}>
+              </input>
+            </label>
           </div>
         </div>
         <Link to='/dashboard'>
